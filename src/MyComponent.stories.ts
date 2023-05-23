7e63e425cfb559ab53de63ba007abbb4117f366f
Nuria <soriano.nuria@gmail.com>
2023-05-23 10:55:56 +0200

    style: fix lint issues

--- src/MyComponent.stories.ts
+++ src/MyComponent.stories.ts
@@ -1,17 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { MyComponent } from './MyComponent';
+import { MyComponent } from "./MyComponent";
 
 const meta = {
-  title: 'MyComponent',
-  component: MyComponent,
+	title: "MyComponent",
+	component: MyComponent,
 } satisfies Meta<typeof MyComponent>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    title: "My Component"
-  },
+	args: {
+		title: "My Component",
+	},
 };
--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,9 +1,6 @@
 import "./MyComponent.scss";
 
-
-export function MyComponent({title}: {
-	title: string
-}) {
+export function MyComponent({ title }: { title: string }) {
 	return (
 		<div className="myComponent">
 			<h1>{title}</h1>
